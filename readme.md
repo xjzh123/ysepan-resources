900db01b74101f67fa5f12e1691b55f980ded151
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-03-04 20:02:25 +0800

    icon

---
+++ readme.md
@@ -0,0 +1,3 @@
+# YSEPAN resources
+
+Thanks to [Cmd1152](http://cmd1152.ysepan.com/) for providing some code.
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("%c脚本加载成功", "color: #fff; border-radius: 5px; padding: 10px 25px;background: linear-gradient(315deg, #1fd1f9 0%, #b621fe 74%)");
+console.log("%c脚本加载成功", "color: #fff; border-radius: 5px; padding: 10px 25px;background: linear-gradient(315deg, #1fd1f9 0%, #b621fe 74%)")
+
+/* Custom icon */
+
+let link = document.createElement('link')
+link.href = 'https://things-ysepan-resources.netlify.app/icon.png'
+link.rel = 'icon'
+document.head.appendChild(link)
